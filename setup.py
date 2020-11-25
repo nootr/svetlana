c26564c8b1d67207402ea39bb2fda7311ce64fd7
Joris Hartog <jhartog@hostnet.nl>
2020-11-25 19:42:35 +0100

    Bump version to v1.3.0
    Changelog:
    
    * Change embed title format

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='svetlana',
-    version='1.2.0',
+    version='1.3.0',
     description='A WebDiplomacy notification bot for Discord.',
     long_description=long_description,
     author='Joris Hartog',
