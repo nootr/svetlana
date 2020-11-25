88b00e0720ff046f1244720375e9e0a88d3bf082
Joris Hartog <jhartog@hostnet.nl>
2020-11-25 22:41:26 +0100

    Bump version to v1.3.1
    Changelog:
    
    * Improved the poller by remove the dependency on pure luck
    * Updated exception logging to include traceback

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='svetlana',
-    version='1.3.0',
+    version='1.3.1',
     description='A WebDiplomacy notification bot for Discord.',
     long_description=long_description,
     author='Joris Hartog',
